fcb60642c2dc8a4a705e4f3651f505b9234d4efe
Duncan Leo <hello@duncanleo.me>
2019-10-06 18:15:08 +0800

    add working sensor reading

--- cmd/hc-dht/cmd.go
+++ cmd/hc-dht/cmd.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"time"
 
+	"github.com/MichaelS11/go-dht"
 	"github.com/brutella/hc/service"
 
 	"github.com/brutella/hc"
 	"github.com/brutella/hc/accessory"
-	hcLog "github.com/brutella/hc/log"
 )
 
 func main() {
-	hcLog.Debug.Enable()
-
 	var name = flag.String("name", "DHT22", "name of the sensor to display in HomeKit")
 	var manufacturer = flag.String("manufacturer", "Aosong Electronics", "manufacturer of the sensor")
 	var model = flag.String("model", "DHT22", "model number of the sensor")
 	var serial = flag.String("serial", "0000", "serial number of the sensor")
 	var pin = flag.String("pin", "00102003", "PIN number to pair the HomeKit accessory")
+	var gpioPin = flag.Int("gpioPin", 4, "GPIO pin of the DHT22")
+	var storagePath = flag.String("storagePath", "hc-dht-data", "path to store data")
 
 	info := accessory.Info{
 		Name:         *name,
@@ -27,25 +29,44 @@ func main() {
 		SerialNumber: *serial,
 	}
 
+	// DHT Setup
+	err := dht.HostInit()
+	if err != nil {
+		log.Panic("HostInit error: ", err)
+		return
+	}
+	dhtSensor, err := dht.NewDHT(fmt.Sprintf("GPIO%d", *gpioPin), dht.Celsius, "")
+	if err != nil {
+		log.Panic("DHT init error: ", err)
+		return
+	}
+
+	var dhtStop = make(chan struct{})
+	var dhtStopped = make(chan struct{})
+	var humidity float64
+	var temperature float64
+
+	go dhtSensor.ReadBackground(&humidity, &temperature, 20*time.Second, dhtStop, dhtStopped)
+
 	ac := accessory.New(info, accessory.TypeSensor)
 
 	tempSensor := service.NewTemperatureSensor()
 	tempSensor.CurrentTemperature.OnValueGet(func() interface{} {
-		return 99
+		return temperature
 	})
 
 	ac.AddService(tempSensor.Service)
 
 	humiditySensor := service.NewHumiditySensor()
 	humiditySensor.CurrentRelativeHumidity.OnValueGet(func() interface{} {
-		return 2
+		return humidity
 	})
 
 	ac.AddService(humiditySensor.Service)
 
 	hcConfig := hc.Config{
 		Pin:         *pin,
-		StoragePath: "storage",
+		StoragePath: *storagePath,
 	}
 
 	t, err := hc.NewIPTransport(hcConfig, ac)
@@ -58,4 +79,8 @@ func main() {
 	})
 
 	t.Start()
+
+	close(dhtStop)
+
+	<-dhtStopped
 }
