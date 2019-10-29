f9237adcfe9c4a8956ef4111e71cca75c5cd67ed
Duncan Leo <hello@duncanleo.me>
2019-10-29 16:52:17 +0800

    switch to HTTP requests

--- cmd/hc-temp-sensor/cmd.go
+++ cmd/hc-temp-sensor/cmd.go
@@ -2,27 +2,42 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io/ioutil"
 	"log"
-	"time"
+	"net/http"
 
-	"github.com/MichaelS11/go-dht"
+	"github.com/brutella/hc/characteristic"
 	"github.com/brutella/hc/service"
+	"github.com/tidwall/gjson"
 
 	"github.com/brutella/hc"
 	"github.com/brutella/hc/accessory"
 )
 
+func makeHTTPRequest(url string) ([]byte, error) {
+	log.Printf("GET '%s'", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		return make([]byte, 0), nil
+	}
+	log.Println(resp.Status)
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
 func main() {
-	var name = flag.String("name", "DHT22", "name of the sensor to display in HomeKit")
+	var name = flag.String("name", "hc-temp-sensor", "name of the sensor to display in HomeKit")
 	var manufacturer = flag.String("manufacturer", "Aosong Electronics", "manufacturer of the sensor")
 	var model = flag.String("model", "DHT22", "model number of the sensor")
 	var serial = flag.String("serial", "0000", "serial number of the sensor")
 	var pin = flag.String("pin", "00102003", "PIN number to pair the HomeKit accessory")
-	var gpioPin = flag.Int("gpioPin", 4, "GPIO pin of the DHT22")
-	var storagePath = flag.String("storagePath", "hc-dht-data", "path to store data")
-	var useFahrenheit = flag.Bool("f", false, "add this flag to use Fahrenheit")
-	var fetchIntervalSec = flag.Int("fetchInterval", 20, "time interval in seconds to fetch data")
+	var port = flag.String("port", "50004", "Port number for the HomeKit accessory")
+	var storagePath = flag.String("storagePath", "hc-temp-sensor-data", "path to store data")
+
+	var url = flag.String("url", "", "URL to fetch temperature")
+	var isHumidityEnabled = flag.Bool("humidity", false, "whether to enable humidity")
+	var tempJSONPath = flag.String("tempJSONPath", ".temperature", "JSON path to the temperature value")
+	var humJSONPath = flag.String("humJSONPath", ".humidity", "JSON path to the humidity value")
 
 	flag.Parse()
 
@@ -33,49 +48,62 @@ func main() {
 		SerialNumber: *serial,
 	}
 
-	var tempUnit = dht.Celsius
-	if *useFahrenheit {
-		tempUnit = dht.Fahrenheit
-	}
-
-	// DHT Setup
-	err := dht.HostInit()
-	if err != nil {
-		log.Panic("HostInit error: ", err)
-		return
-	}
-	dhtSensor, err := dht.NewDHT(fmt.Sprintf("GPIO%d", *gpioPin), tempUnit, "")
-	if err != nil {
-		log.Panic("DHT init error: ", err)
-		return
-	}
-
-	var dhtStop = make(chan struct{})
-	var dhtStopped = make(chan struct{})
-	var humidity float64
-	var temperature float64
-
-	go dhtSensor.ReadBackground(&humidity, &temperature, time.Duration(*fetchIntervalSec)*time.Second, dhtStop, dhtStopped)
-
 	ac := accessory.New(info, accessory.TypeSensor)
 
 	tempSensor := service.NewTemperatureSensor()
+
+	tempStatusActive := characteristic.NewStatusActive()
+	tempSensor.AddCharacteristic(tempStatusActive.Characteristic)
+
+	tempStatusFault := characteristic.NewStatusFault()
+	tempSensor.AddCharacteristic(tempStatusFault.Characteristic)
+
 	tempSensor.CurrentTemperature.OnValueGet(func() interface{} {
-		return temperature
+		log.Println("tempSensor.CurrentTemperature.OnValueGet")
+		tempStatusActive.SetValue(true)
+		data, err := makeHTTPRequest(*url)
+		tempStatusActive.SetValue(false)
+		if err != nil {
+			log.Println(err)
+			tempStatusFault.SetValue(characteristic.StatusFaultGeneralFault)
+			return nil
+		}
+		tempStatusFault.SetValue(characteristic.StatusFaultNoFault)
+		return gjson.Get(string(data), *tempJSONPath).Float()
 	})
 
 	ac.AddService(tempSensor.Service)
 
-	humiditySensor := service.NewHumiditySensor()
-	humiditySensor.CurrentRelativeHumidity.OnValueGet(func() interface{} {
-		return humidity
-	})
+	if *isHumidityEnabled {
+		humiditySensor := service.NewHumiditySensor()
 
-	ac.AddService(humiditySensor.Service)
+		humidityStatusFault := characteristic.NewStatusFault()
+		humiditySensor.AddCharacteristic(humidityStatusFault.Characteristic)
+
+		humidityStatusActive := characteristic.NewStatusActive()
+		humiditySensor.AddCharacteristic(humidityStatusActive.Characteristic)
+
+		humiditySensor.CurrentRelativeHumidity.OnValueGet(func() interface{} {
+			log.Println("humiditySensor.CurrentRelativeHumidity.OnValueGet")
+			humidityStatusActive.SetValue(true)
+			data, err := makeHTTPRequest(*url)
+			humidityStatusActive.SetValue(false)
+			if err != nil {
+				log.Println(err)
+				humidityStatusFault.SetValue(characteristic.StatusFaultGeneralFault)
+				return nil
+			}
+			humidityStatusFault.SetValue(characteristic.StatusFaultNoFault)
+			return gjson.Get(string(data), *humJSONPath).Float()
+		})
+
+		ac.AddService(humiditySensor.Service)
+	}
 
 	hcConfig := hc.Config{
 		Pin:         *pin,
 		StoragePath: *storagePath,
+		Port:        *port,
 	}
 
 	t, err := hc.NewIPTransport(hcConfig, ac)
@@ -88,8 +116,4 @@ func main() {
 	})
 
 	t.Start()
-
-	close(dhtStop)
-
-	<-dhtStopped
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/duncanleo/hc-temp-sensor
 go 1.12
 
 require (
-	github.com/MichaelS11/go-dht v0.0.0-20181004212404-be44b9ee7fec
 	github.com/brutella/hc v1.2.0
 	github.com/miekg/dns v1.1.17 // indirect
+	github.com/savaki/jq v0.0.0-20161209013833-0e6baecebbf8
+	github.com/tidwall/gjson v1.3.2
 	golang.org/x/crypto v0.0.0-20190911031432-227b76d455e7 // indirect
 	golang.org/x/net v0.0.0-20190909003024-a7b16738d86b // indirect
 	golang.org/x/sys v0.0.0-20190910064555-bbd175535a8b // indirect
-	periph.io/x/periph v3.6.2+incompatible // indirect
 )
